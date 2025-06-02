3e6afbca2d77c8364d0a8cf1504985561f39b2f0
EliasFritioff <142496350+EliasFritioff@users.noreply.github.com>
2025-06-02 13:34:11 +0200

    Klart

--- QuizGame/obj/Debug/net8.0/QuizGame.AssemblyInfo.cs
+++ QuizGame/obj/Debug/net8.0/QuizGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf241cb29d9a0acb20cd858912996b47bee75a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
